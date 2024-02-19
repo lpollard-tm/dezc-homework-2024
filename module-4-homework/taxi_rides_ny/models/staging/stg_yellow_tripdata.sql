bd247a32f50605ed560faa1c5537019bca3644c2
Lottie Jane Pollard <l.pollard@techmodal.com>
2024-02-19 18:50:21 +0000

    commit

--- module-4-homework/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ module-4-homework/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -2,13 +2,13 @@
 
 with
     tripdata as (
-        select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn  {# rn = rownumber #}
-        from {{ source("staging", "green_tripdata") }}
+        select *, row_number() over (partition by vendorid, tpep_pickup_datetime) as rn
+        from {{ source("staging", "yellow_tripdata") }}
         where vendorid is not null
     )
 select
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(["vendorid", "lpep_pickup_datetime"]) }}
+    {{ dbt_utils.generate_surrogate_key(["vendorid", "tpep_pickup_datetime"]) }}
     as tripid,
     {{ dbt.safe_cast("vendorid", api.Column.translate_type("integer")) }} as vendorid,
     {{ dbt.safe_cast("ratecodeid", api.Column.translate_type("integer")) }}
@@ -19,15 +19,16 @@ select
     as dropoff_locationid,
 
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- trip info
     store_and_fwd_flag,
     {{ dbt.safe_cast("passenger_count", api.Column.translate_type("integer")) }}
     as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    {{ dbt.safe_cast("trip_type", api.Column.translate_type("integer")) }} as trip_type,
+    -- yellow cabs are always street-hail
+    1 as trip_type,
 
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -35,7 +36,7 @@ select
     cast(mta_tax as numeric) as mta_tax,
     cast(tip_amount as numeric) as tip_amount,
     cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
+    cast(0 as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
     coalesce(
@@ -45,5 +46,5 @@ select
 from tripdata
 where rn = 1
 
--- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+-- dbt build --select <model.sql> --vars '{'is_test_run: false}'
 {% if var("is_test_run", default=true) %} limit 100 {% endif %}
