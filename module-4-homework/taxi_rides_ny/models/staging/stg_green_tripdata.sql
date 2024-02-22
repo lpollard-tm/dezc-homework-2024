bdccbeff64b2b39d2687911d14719e69864e9c43
Lottie Jane Pollard <l.pollard@techmodal.com>
2024-02-22 10:41:38 +0000

    test

--- module-4-homework/taxi_rides_ny/models/staging/stg_green_tripdata.sql
+++ module-4-homework/taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -3,7 +3,7 @@
 with
     tripdata as (
         select *, row_number() over (partition by vendorid, lpep_pickup_datetime) as rn  {# rn = rownumber #}
-        from {{ source("staging", "green_tripdata") }}
+        from {{ source("staging", "") }}
         where vendorid is not null
     )
 select
